38ba62afa26ef23f6f157b6a2d9c34ee78decb3a
Armag3ddon <Armag3ddon@web.de>
2016-10-29 21:17:57 +0200

    grafiken

--- js/entity/player.js
+++ js/entity/player.js
@@ -1,6 +1,8 @@
-define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic', 'lib/animation', 'lib/velociraptor'],
-	function(Entity, V2, colors, RectEntity, graphics, Animation, Velociraptor) {
+define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic', 'lib/animation', 'lib/velociraptor', 'core/graphic'],
+	function(Entity, V2, colors, RectEntity, graphics, Animation, Velociraptor, graphics) {
 
+		graphics.add('img/adult_spritesheet_40x40.png');
+		graphics.add('img/child_spritesheet_40x40.png');
 
 		function Player(pos, collider, character) {
 			Entity.call(this, pos);
@@ -8,9 +10,9 @@ define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic',
 			this.velocity = new V2(0,0);
 
 			switch(character) {
-				case 'y': this.img =  new RectEntity(Zero(), new V2(40, 40), colors.player_y); break;
-				case 'a': this.img =  new RectEntity(Zero(), new V2(40, 80), colors.player_a); break;
-				case 'e': this.img =  new RectEntity(Zero(), new V2(40, 80), colors.player_e); break;
+				case 'y': this.img =  new Animation('img/child_spritesheet_40x40.png', new V2(-20,-40), new V2(8, 3), 100, true); this.size = new V2(40, 40); break;
+				case 'a': this.img =  new Animation('img/adult_spritesheet_40x40.png', new V2(-20,-10), new V2(8, 3), 100, true); this.size = new V2(40, 70); break;
+				case 'e': this.img =  new RectEntity(Zero(), new V2(40, 70), colors.player_e); this.size = new V2(40, 70); break;
 			}
 
 			this.add(this.img);
@@ -20,6 +22,9 @@ define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic',
 
 			this.grounded = false;
 			this.collider = collider(this);
+
+			this.isWalking = false;
+			this.isJumping = false;
 
 			this.velociraptor = new Velociraptor();
 			if(character == 'a')
@@ -37,6 +42,29 @@ define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic',
 
 			if(c.y) this.velocity.y = 0;
 			if(c.x) this.velocity.x = 0;
+
+			if (this.character == 'e') return;
+
+			if (!this.grounded) {
+				if (!this.isJumping) {
+					this.isWalking = false;
+					this.img.state = 2;
+				}
+			} else if (this.velocity.x != 0) {
+				if (this.grounded && !this.isWalking) {
+					this.isWalking = true;
+					this.img.state = 1;
+				}
+			} else if(this.isWalking) {
+				this.isWalking = false;
+				this.img.state = 0;
+			}
+			if (this.isWalking || this.isJumping) {
+				if (this.velocity.x < 0)
+					this.img.flip = -1;
+				else
+					this.img.flip = 1;
+			}
 		};
 
 		Player.prototype.down = function(key) {
@@ -77,6 +105,10 @@ define(['basic/entity', 'geo/v2', 'config/colors', 'basic/rect', 'core/graphic',
 		Player.prototype.fadeIn = function () {
 			this.img.alpha = 1;
 		};
+
+/*		Player.prototype.onDraw = function (ctx) {
+			ctx.strokeRect(0,0, 40,80);
+		}*/
 
 		return Player;
 	}
--- js/lib/animation.js
+++ js/lib/animation.js
@@ -1,5 +1,5 @@
-define(['basic/entity', 'core/graphic', 'geo/v2'],
-		function (Entity, graphics, V2) {
+define(['basic/entity', 'core/graphic', 'geo/v2', 'basic/rect'],
+		function (Entity, graphics, V2, Rect) {
 			function Animation( img, pos, frames, speed, loop ) {
 				this.frames = typeof frames == 'number' ? new V2(frames, 1) : frames;
 				this.img = graphics[img];
@@ -10,8 +10,10 @@ define(['basic/entity', 'core/graphic', 'geo/v2'],
 				this.frame = 0;
 				this.state = 0;
 				this.alpha = 1;
+				this.flip = 1;
 
 				Entity.call(this, pos, new V2(this.img.width / this.frames.x, this.img.height / this.frames.y ));
+				//this.add(new Rect(this.position, this.size));
 			}
 
 			Animation.prototype = new Entity();
@@ -29,7 +31,11 @@ define(['basic/entity', 'core/graphic', 'geo/v2'],
 
 			Animation.prototype.onDraw = function(ctx) {
 				ctx.globalAlpha = this.alpha;
-				ctx.drawImage( this.img, this.frame*this.size.x, this.state*this.size.y, this.size.x, this.size.y, 0, 0, this.size.x, this.size.y );
+				ctx.scale(this.flip, 1);
+				var trans = 0;
+				if (this.flip == -1)
+					trans = -this.size.x;
+				ctx.drawImage( this.img, this.frame*this.size.x, this.state*this.size.y, this.size.x, this.size.y, trans, 0, this.size.x, this.size.y );
 				ctx.globalAlpha = 1;
 			};
 
