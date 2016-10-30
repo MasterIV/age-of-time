9ca91be7c2a9d8c021c61a2f5d8f460d33ffbd64
Kevin Kub <github@kekub.de>
2016-10-30 14:40:26 +0100

    Added image to credits

--- js/scenes/credits.js
+++ js/scenes/credits.js
@@ -1,11 +1,10 @@
-define(['lib/scene', 'entity/back', 'basic/text', 'geo/v2'],
-		function(Scene, BackButton, TextEntity, V2) {
+define(['lib/scene', 'entity/back', 'basic/text', 'geo/v2', 'basic/image', 'core/graphic'],
+		function(Scene, BackButton, TextEntity, V2, Image, graphics) {
+			graphics.add('img/main-screen-bg.jpg');
+
 			function CreditsScene() {
 				Scene.call(this);
-				this.center(new TextEntity(new V2(0, 100), "Max Mustermann"));
-				this.center(new TextEntity(new V2(0, 200), "Erica Mustemann"));
-				this.center(new TextEntity(new V2(0, 300), "Gunda Gamedesigner"));
-				this.center(new TextEntity(new V2(0, 400), "Peter Programmierer"));
+				this.add(new Image(new V2(0, 0), 'img/main-screen-bg.jpg'));
 				this.add(BackButton('menu', 'right'));
 			}
 
