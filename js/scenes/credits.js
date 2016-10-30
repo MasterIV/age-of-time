71eb118b49b7a179c5484ec7d26d042167174291
Tobias Rojahn <tobias@rojahn.org>
2016-10-30 14:52:54 +0100

    credits

--- js/scenes/credits.js
+++ js/scenes/credits.js
@@ -1,10 +1,10 @@
 define(['lib/scene', 'entity/back', 'basic/text', 'geo/v2', 'basic/image', 'core/graphic'],
 		function(Scene, BackButton, TextEntity, V2, Image, graphics) {
-			graphics.add('img/main-screen-bg.jpg');
+			graphics.add('img/credits.jpg');
 
 			function CreditsScene() {
 				Scene.call(this);
-				this.add(new Image(new V2(0, 0), 'img/main-screen-bg.jpg'));
+				this.bg = 'img/credits.jpg';
 				this.add(BackButton('menu', 'right'));
 			}
 
