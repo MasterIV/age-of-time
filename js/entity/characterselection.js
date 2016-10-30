cd8d8ae72a6a2244c75a664c7579e6a546046edf
Kevin Kub <github@kekub.de>
2016-10-30 13:07:00 +0100

    Morph in character selection

--- js/entity/characterselection.js
+++ js/entity/characterselection.js
@@ -1,7 +1,7 @@
 define([
-	'basic/entity', 'basic/button', 'config/colors', 'geo/v2', 'core/graphic', 'basic/layout', 'config/screen'
+	'basic/entity', 'basic/button', 'config/colors', 'geo/v2', 'core/graphic', 'basic/layout', 'config/screen', 'basic/morph', 'definition/easing'
 ], function (
-	Entity, Button, colors, V2, graphics, Layout, screen
+	Entity, Button, colors, V2, graphics, Layout, screen, Morph, EASING
 ) {
 	graphics.add('img/character_selection_adult.png');
 	graphics.add('img/character_selection_adult_glow.png');
@@ -14,25 +14,31 @@ define([
 		Entity.call(this, Zero(), new V2(screen.w, screen.h));
 		var self = this;
 
-		var hLayout = new Layout.horizontal(new V2(0,180), 20, 10);
+		this.backgroundOpacity = 0;
+		this.hLayout = new Layout.horizontal(new V2(0,-360), 20, 10);
+		
+		this.add(new Morph({backgroundOpacity: .7}, 4000, EASING.INCUBIC, function(){
+			self.add(new Morph({hLayout: {position: {y: 180}}}, 1600, EASING.OUTELASTIC));
+		}));
+			
 
 		if(!data.y)
-			hLayout.add(Button.create(new V2(40, 400), function() {
+			this.hLayout.add(Button.create(new V2(40, 400), function() {
 				self.select('y');
 			}).img('img/character_selection_child.png').hoverImg('img/character_selection_child_glow.png'));
 
 		if(!data.a)
-			hLayout.add(Button.create(new V2(340, 400), function() {
+			this.hLayout.add(Button.create(new V2(340, 400), function() {
 				self.select('a');
 			}).img('img/character_selection_adult.png').hoverImg('img/character_selection_adult_glow.png'));
 
 		if(!data.e)
-			hLayout.add(Button.create(new V2(640, 400), function() {
+			this.hLayout.add(Button.create(new V2(640, 400), function() {
 				self.select('e');
 			}).img('img/character_selection_old.png').hoverImg('img/character_selection_old_glow.png'));
 
-		hLayout.align("center");
-		this.center(hLayout);
+		this.hLayout.align("center");
+		this.center(this.hLayout);
 	}
 
 	CharacterSelction.prototype = new Entity();
@@ -43,7 +49,7 @@ define([
 	};
 
 	CharacterSelction.prototype.onDraw = function (ctx) {
-		ctx.fillStyle = 'rgba(0,0,0,.7)';
+		ctx.fillStyle = 'rgba(0,0,0,'+ this.backgroundOpacity +')';
 		ctx.fillRect(0,0,this.size.x, this.size.y);
 	};
 
