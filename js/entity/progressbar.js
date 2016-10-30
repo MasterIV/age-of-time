37b82d005ede44f9e875c6255a96a2524500a35c
Kevin Kub <github@kekub.de>
2016-10-30 14:58:28 +0100

    Cancel button.

--- js/entity/progressbar.js
+++ js/entity/progressbar.js
@@ -5,6 +5,7 @@ define([
 ) {
 	graphics.add('img/timeline.png');
 	graphics.add('img/timeline_full.png');
+	graphics.add('img/button_cancel.png');
 
 	function Progressbar( pos ) {
 		Entity.call(this, pos);
@@ -13,13 +14,19 @@ define([
 		this.topPosition = 730;
 		this.add(new Morph({topPosition: 660}, 1000, EASING.INOUTCUBIC));
 
-		this.bar = Button.create(new V2(436, this.topPosition), function() {
+		this.bar = Button.create(new V2(469, this.topPosition), function() {
 			self.setProgress(1);
 			self.parent.delta = self.parent.duration;
 		}).img('img/timeline.png');
 		this.add(this.bar);
 
-		this.progressImage = new PartialImage(new V2(436, this.topPosition), 'img/timeline_full.png', 1, 0);
+		this.cancel = Button.create(new V2(403, this.topPosition), function() {
+			self.setProgress(1);
+			self.parent.delta = self.parent.duration;
+		}).img('img/button_cancel.png');
+		this.add(this.cancel);
+
+		this.progressImage = new PartialImage(new V2(469, this.topPosition), 'img/timeline_full.png', 1, 0);
 		this.add(this.progressImage);
 
 		this.setProgress(0);
@@ -28,6 +35,7 @@ define([
 	Progressbar.prototype = new Entity();
 
 	Progressbar.prototype.onUpdate = function() {
+		this.cancel.position.y = this.topPosition;
 		this.bar.position.y = this.topPosition;
 		this.progressImage.position.y = this.topPosition;
 		var progress = this.parent.delta / this.parent.duration;
